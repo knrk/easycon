0815d5c3aa087856f4b0a33a78037aa2c04480df
Lukas Konarik <lukas.konarik@solarwinds.com>
2018-08-07 16:15:59 +0200

    Daily update

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,31 @@
-particlesJS.load('particles-js', './particles.json', function() {
-    console.log('callback - particles.js config loaded');
+document.addEventListener('DOMContentLoaded', () => {
+
+    particlesJS.load('particles-js', './particles.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
+
+
+
+    const sections = document.querySelectorAll('main .boxes a');
+    const contents = document.querySelectorAll('main .content');
+    const clearActive = (list, contentboxes) => {
+        list.forEach(item => {
+            item.classList.remove('active');
+        });
+        contentboxes.forEach(content => {
+            content.classList.add('hidden');
+        })
+    }
+    sections.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            clearActive(sections, contents);
+            event.target.classList.add('active');
+            const activate = Array.from(event.target.classList).filter(cn => cn !== 'box');
+            document.querySelector(`#${activate[0]}-content`).classList.remove('hidden');
+        });
+    });
+
+    new Glide('#webapps .slider').mount();
+    new Glide('#controlling .slider').mount();
+
 });
